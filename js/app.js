a723a50e73912e2046ef07e814187d9126e6a89e
ethanbeauregard <ethantbeauregard@gmail.com>
2019-06-14 14:18:08 -0400

    Added chnages for cloud transfer over to netlify

--- js/app.js
+++ js/app.js
@@ -5,3 +5,14 @@ var clickedBtn = function() {
 }
 menuBtn[0].addEventListener('click', clickedBtn);
 console.log(menu-btn[0]);
+
+// smooth scroll
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	}  );
+});
